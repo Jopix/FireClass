a68de316c324c60c07c2eb17b732bb6cb9bc5ae0
Jopix <kkil58@126.com>
2014-05-07 11:25:42 +0800

    更新工程配置

--- FireClass/FCmain/platform/FCToC.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "FCToC.h"
-#include "FCSocketManager.h"
-
-static void addDevice(const char * dev)
-{
-	FCSocketManager::shareInstance()->getSocket()->setText(dev);
-}
--- FireClass/FCmain/platform/android/FCClientSocket_Java.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <jni.h>
-#include "../FCToC.h"
-
-JNIEXPORT void JNICALL Java_com_example_fileclass_FCClientSocket_1Java_addDevice
-  (JNIEnv * env, jclass, jstring){
-  	const char* pdata = env->GetStringUTFChars(jstring,NULL);
-  	addDevice(pdata);
-  }
